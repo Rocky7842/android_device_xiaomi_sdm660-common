12c69c9b478b63e0d64c9a54a8d87971a2a2f01d
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    sdm660-common: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- sdm660.mk
+++ sdm660.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
