ef8b76a2ac46d7c98b27cf9327b02b886640555b
Rocky7842 <eric.rocky7842@gmail.com>
2025-03-24 00:42:32 +0800

    sdm660-common: Build missing libs for camera blobs
    Change-Id: I799e77672b75ab1b72ce6128c6558436d1988a31

--- sdm660.mk
+++ sdm660.mk
@@ -115,6 +115,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
+    libpiex.vendor \
+    libpng.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Configstore
