fcc5979088d0a52537b527141fcb50391272e595
Rocky7842 <eric.rocky7842@gmail.com>
2025-03-24 18:31:56 +0800

    sdm660-common: Build libpower.vendor for sensors
    Change-Id: Ie8f781a5b6a88dacb1979b8a317731c273adbdb3

--- sdm660.mk
+++ sdm660.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     libdisplayconfig \
     libgralloc.qti \
+    libpower.vendor \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
