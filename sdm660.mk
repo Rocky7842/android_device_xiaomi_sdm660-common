f050a9e8e1252cd18e2fd977645ec132f387ad4d
Rocky7842 <eric.rocky7842@gmail.com>
2025-04-11 19:16:13 +0800

    sdm660-common: Switch to common NoCutoutOverlay
    Change-Id: Iab93d7578b4389bfbecaf8896ff91935f8736db9

--- sdm660.mk
+++ sdm660.mk
@@ -340,6 +340,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+    
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
