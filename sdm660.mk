0e79309f1e52c710b322b3ffc37d1fe7510543ff
Rocky7842 <eric.rocky7842@gmail.com>
2025-08-05 19:20:36 +0800

    sdm660-common: Build Dolby audio
    Change-Id: Iaacfccd282973328c40c0477bbc9b727831d8713

--- sdm660.mk
+++ sdm660.mk
@@ -105,6 +105,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Audio (Dolby)
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
