9d55d0862df83b33ab73dafe229a88421f8f0682
Rocky7842 <eric.rocky7842@gmail.com>
2026-01-28 19:24:55 +0800

    sdm660-common: Convert soong config to bool
    Change-Id: Idf19faba96ed2a8b4e90aa21601fd441efa86145

--- sdm660.mk
+++ sdm660.mk
@@ -328,7 +328,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Mlipay
 PRODUCT_PACKAGES += \
