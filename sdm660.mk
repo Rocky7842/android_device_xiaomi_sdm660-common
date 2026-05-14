311823968a08f313be5c977f88eb44acde1de71d
Rocky7842 <eric.rocky7842@gmail.com>
2026-05-14 16:05:33 +0800

    sdm660-common: Import dcvs init script
    * From LA.UM.9.2.1.r1-08000-sdm660.0
    
    Change-Id: If8b12b0bf2b407050b12f72bdfbf9648171c9f80

--- sdm660.mk
+++ sdm660.mk
@@ -271,7 +271,8 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sh
+    init.qcom.sh \
+    init.qti.dcvs.sh
 
 PRODUCT_PACKAGES += \
     init.fingerprint.rc \
